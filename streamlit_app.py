25f9a8a143847db078cfd203d0714f15eea0b9f9
ananddivakaruni <ananddivakaruni@gmail.com>
2024-06-12 15:52:51 -0400

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +10,10 @@ st.write("""Choose the fruits you want in your custom smoothie!""")
 name_in_order = st.text_input('Name on Smoothie:')
 st.write("The name on your smoothie will be:", name_in_order)
 
-session = get_active_session()
+cnx= st.connection("snowflake")
+session = cnx.session()
+# session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 # st.dataframe(data=my_dataframe,use_container_width=True)
 ingredients_list = st.multiselect(
